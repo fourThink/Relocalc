0d4c4be61c2b97ba33f7c2614b3ae716c4c96178
Richard Van Camp <richard.vancamp@tut.fi>
2015-07-16 10:04:50 -0500

    fixed spelling error

--- client/app/index.js
+++ client/app/index.js
@@ -9,7 +9,7 @@ App.controller = function () {}
 App.view = function (ctrl) {
   return [
     m('h1', 'Relocalc'),
-    m.component(MyComponent, { title: 'No the risks before relocating!' })
+    m.component(MyComponent, { title: 'Know the risks before relocating!' })
   ]
 }
 
