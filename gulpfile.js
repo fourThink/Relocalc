79362df2a75d39e7bbbdc924c0d03cc29fc1f18d
Richard Van Camp <richard.vancamp@tut.fi>
2015-07-16 07:10:20 -0500

    added new gulp tasks and jshint rc & ignore files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+//vinyle source needed becasuee browserify uses chunks while gulp uses streams
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+//reruns gulp files after changes
 var watchify = require('watchify');
 var notifier = require('node-notifier');
 var server = require('gulp-server-livereload');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var jshint = require('gulp-jshint');
+var map = require('map-stream');
+
+/**
+ * Notifies you of error lines and types with desktop message
+ * @param error
+ */
 
 var notify = function(error) {
   var message = 'In: ';
@@ -31,11 +40,14 @@ var notify = function(error) {
   notifier.notify({title: title, message: message});
 };
 
+/**
+ *  bundler object configures build with browserify
+ */
+
 var bundler = watchify(browserify({
-  entries: ['client/app/index.js'],
-  //dest: '/js',
-  outputName: 'app.js'
-  //extensions: ['.js'],
+  //file that kicks off app
+  entries: ['./client/app/index.js'],
+  extensions: ['.js'],
   debug: true,
   cache: {},
   packageCache: {},
@@ -46,8 +58,9 @@ function bundle() {
   return bundler
       .bundle()
       .on('error', notify)
+      //combines all files here
       .pipe(source('main.js'))
-      .pipe(gulp.dest('./'))
+      .pipe(gulp.dest('./build'))
 }
 bundler.on('update', bundle)
 
@@ -72,6 +85,10 @@ gulp.task('serve', function(done) {
       }));
 });
 
+/**
+ *  compiles sass files into css
+ */
+
 gulp.task('sass', function () {
   gulp.src('/client/public/sass/**/*.scss')
       .pipe(sass().on('error', sass.logError))
@@ -79,7 +96,30 @@ gulp.task('sass', function () {
       .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['build', 'serve', 'sass', 'watch']);
+/**
+ *  formats and logs jshint warnings
+ */
+
+var reporter = map(function (file, cb) {
+  if (!file.jshint.success) {
+    console.log('JSHINT fail in '+file.path);
+    file.jshint.results.forEach(function (err) {
+      if (err) {
+        console.log(' '+file.path + ': line ' + err.line + ', col ' + err.character + ', code ' + err.code + ', ' + err.reason);
+      }
+    });
+  }
+  cb(null, file);
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['client/**/**/*', 'server/*'])
+      .pipe(jshint())
+      .pipe(reporter);
+});
+
+gulp.task('test', ['lint']);
+gulp.task('default', ['lint', 'build', 'serve', 'sass', 'watch']);
 
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
